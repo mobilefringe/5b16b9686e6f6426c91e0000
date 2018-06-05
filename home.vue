785401778dc3c2cea2adde7fb7e123da1281e9c9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-05 17:11:33 -0400

    Holt Renfrew Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -65,7 +65,7 @@
         			</div>
         			<div class="top_two hidden_phone col-sm-3">
         				<div class="banner_features"  v-for="feature in banner_features">
-        					<router-link :to="feature.url" class="hvr-grow-shadow">
+        					<router-link :to="feature.url">
         						<img class="top_image" :src='feature.image_url' :alt="'Click here for more info on ' +feature.name"/>
         						<div class="feature_overlay text-uppercase ">{{feature.name}}</div>
         					</router-link>
