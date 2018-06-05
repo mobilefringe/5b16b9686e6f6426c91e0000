b3044f0eee73ec2793cffa460479b8da22a2df5c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-05 17:19:29 -0400

    Holt Renfrew Centre 1.0  /find_us.vue

---
+++ find_us.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="page_container" id="find_us_container">
+		<div class="margin_25_across padding_top_40">
+			<div class="row text-left">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1700.7834802639834!2d-79.76570131628588!3d43.230419353327626!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc2187d635df8645b!2sEastgate+Square+Fortinos!5e0!3m2!1sen!2sca!4v1515083028714" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<div class="page_header all_caps double_border_top double_border_bottom">
+            			<div class="page_container text_left">How to Get Here</div>
+            		</div>
+					<div  class="directions">
+					    <p>Eastgate Square is easy to find and there is plenty of parking once you arrive!</p>
+    					<p><strong>From the Queen Elizabeth Way (QEW)</strong></p>
+    					<p>Simply follow the QEW, take the Centennial Pkwy N. exit, and continue south. We will be on the right hand side of the road.</p>
+    					<p><strong>From the Red Hill Expressway</strong></p>
+    					<p>Take the Queenston Rd. exit and head eastbound. You will find Eastgate Square on the left hand side, just before the intersection with Centennial Pkwy N.</p>
+					</div>
+					
+					
+					<div class="page_header all_caps double_border_top double_border_bottom">
+            			<div class="page_container text_left"> Public Transit</div>
+            		</div>
+            		<div class="directions">
+            		    <p>Eastgate Square is serviced by the Hamilton Street Railway. The following Routes will get you here: 1 (King), B-Line Express, 55 (Stoney Creek Central), 56 (Centennial), and 58 (Stoney Creek Local). Visit <a href="//www.hamilton.ca/CityServices/transit/">www.hamilton.ca/CityServices/transit/</a> for bus maps and schedules.</p>
+            		</div>
+					
+			</div>
+			<div class="padding_top_40"></div>
+		</div>
+	</div>
+</template>
+
+<style>
+    #find_us_container .row{
+        margin-left:inherit;
+    }
+    #find_us_container {
+        margin: 20px auto 0;
+    }
+    #find_us_container .directions {
+        padding: 10px 0;   
+    }
+    .form-group .form-control-feedback{
+        font-size: 12px;
+        top:50px;
+        color: #F44336;
+    }
+</style>
+
+<script>
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", 'vee-validate'], function(Vue, Vuex, moment, tz, VueMoment, Meta, VeeValidate) {
+        Vue.use(Meta);
+        Vue.use(VeeValidate);
+        return Vue.component("find-us-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    success_subscribe: false,
+                    currentPage: null
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                ]),
+            }
+        });
+    });
+</script>
