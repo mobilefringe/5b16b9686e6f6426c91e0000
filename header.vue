af14c1ec77fffce2e45af05f3ae994eb3a48635e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-05 16:48:25 -0400

    Holt Renfrew Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -15,7 +15,7 @@
     			<div class="col-sm-6 hidden_phone">
     				<div class="social-div social_icons">
     					<router-link to="/contact_us" style="font-size:12px; margin-right:10px; color:#666666;display: inline-block;height: 30px;line-height: 30px;">CONTACT US</router-link>
-    					<a href="http://www.facebook.com/eastgatesquaremall" target="_blank" class="social_link_icon social_fb" alt="Eastgate Facebook"><p style="display:none;">Facebook icon</p></a>
+    					<a href="http://www.facebook.com/eastgatesquaremall" target="_blank" class="social_link_icon social_fb" alt="Eastgate Facebook"><i class="fa fa-facebook"></i><p style="display:none;">Facebook icon</p></a>
     					<a href="http://www.twitter.com/EastgateSquare" target="_blank" class="social_link_icon social_twitter" alt="Eastgate Twitter"><p style="display:none;">Twitter icon</p></a>
     				</div>
     				<div class="newsletter-div">
