0230eaaec70110efd4ffc4c5ef2cc62cc0e70982
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-05 17:25:30 -0400

    Holt Renfrew Centre 1.0  /find_us.vue

--- find_us.vue
+++ find_us.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="" id="find_us_container">
 	    <div class="page_header all_caps double_border_bottom">
-			<h2 class="page_container text_left"> Find Us</h2>
+			<h2 class="page_container text_left page_header_title"> Find Us</h2>
 		</div>
 		<div class="page_container margin_25_across padding_top_40">
 			<div class="row text-left">
