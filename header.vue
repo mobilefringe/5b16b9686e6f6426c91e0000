294fe045aed48e010828c921e0b273edd584d974
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-07 13:07:29 -0400

    Holt Renfrew Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -121,7 +121,7 @@
                     // hide dropdown when route changes
                     _.forEach(this.menu_items, function(value, key) {
                         value.show_sub_menu = false;
-                        console.log(this.$route)
+                        console.log(this.$route.path)
                     });
                     this.show_mobile_menu = false; //close menu when navigating to new page
                 },
