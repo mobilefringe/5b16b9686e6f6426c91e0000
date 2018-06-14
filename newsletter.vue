de3148e9937c751d3d78c9df50342db66e343008
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-14 12:27:01 -0400

    Holt Renfrew Centre 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -8,7 +8,7 @@
             </div>
             <div class="row"> 
                 <div class="col-md-12 contact_contents padding_top_20">
-                    <form class="form-horizontal js-cm-form" id="subForm" method="post" @submit.prevent="validateBeforeSubmit">
+                    <form class="form-horizontal js-cm-form" id="subForm"  action="form-submit" @submit.prevent="validateBeforeSubmit">
                         <div class="form-group ">
                             <div class="col-sm-6 col-xs-12" >
                                 <label class="label" for="cm-name">Name</label>
