d29722b4cdc189ff1d57a2de2722b116d5f081e9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-21 11:47:37 -0400

    Holt Renfrew Centre 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -23,7 +23,7 @@
 					    <a v-bind:href="'//'+currentStore.website" target="_blank">Visit Store Site</a>    
 					</div>
 					
-					<div v-if="currentStore.store_hours.length > 0">
+					<div v-if="currentStore.store_hours && currentStore.store_hours.length > 0">
 					    <p class="underline">Store Hours</p>
 					    <ul class="store_details_hours">
                             <li v-if="!hour.is_closed" v-for="hour in hours">
