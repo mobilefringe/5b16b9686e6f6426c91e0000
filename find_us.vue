440411e141d3a37bf0a42a61a0a125564145538b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-07-05 15:58:38 -0400

    Holt Renfrew Centre 1.0  /find_us.vue

--- find_us.vue
+++ find_us.vue
@@ -32,18 +32,6 @@
                     'timezone',
                 ]),
             },
-            // /pages/holtrenfrew-directions
-            created () {
-                host_name = this.property.mm_host.replace("http:", "");
-                this.$store.dispatch('LOAD_PAGE_DATA', {
-                    url: host_name + "/pages/holtrenfrew-directions.json"
-                }).then(response => {
-                    this.currentPage = response.data;
-                }, error => {
-                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                    this.$router.replace({ path: '/'});
-                });
-            }
         });
     });
 </script>
